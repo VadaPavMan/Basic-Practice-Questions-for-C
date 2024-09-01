0893df39d2c3200466f98dfccf69c223e9e7327a
Harsh Rajbhar <119682671+VadaPavMan@users.noreply.github.com>
2024-09-01 16:38:15 +0530

    Update SolutionMoreQ8.c

--- SolutionMoreQ8.c
+++ SolutionMoreQ8.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    float z = 8.9;     // Declare a float variable z and initialize it with 8.9
+    float z = 5.5;     // Declare a float variable z and initialize it with 5.5
     float *p = &z;     // Declare a pointer p that points to z
 
     // Print the value of z using the pointer dereference and the variable name
