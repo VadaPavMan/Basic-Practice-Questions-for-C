d7974a7fbd879916c1ff41703f5ab676cccd5c37
Harsh Rajbhar <119682671+VadaPavMan@users.noreply.github.com>
2024-09-26 19:28:26 +0530

    Update SolutionMoreQ25.c

--- SolutionMoreQ25.c
+++ SolutionMoreQ25.c
@@ -17,13 +17,13 @@ void display(Vehicle car)
 {
     printf("The model name: %s\n", car.model);
     printf("The year of launch: %d\n", car.year);
-    printf("The price: %.2f\n", car.price);
+    printf("The price: $%.2f\n", car.price);
     printf("\n");
 }
 int main()
 {
-    Vehicle car1 = {"lavdekamodel", 1999, 9999.2};
-    Vehicle car2 = {"jhatkabal", 1995, 8888.2};
+    Vehicle car1 = {"Toyota Corolla", 2015, 15000.2};
+    Vehicle car2 = {"Honda Civic", 2020, 20000.2};
 
     display(car1);
     display(car2);
