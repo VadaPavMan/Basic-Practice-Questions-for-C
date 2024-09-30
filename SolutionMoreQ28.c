e36ad64a4b50b44d56768a8de7dcc19df084924c
Harsh Rajbhar <119682671+VadaPavMan@users.noreply.github.com>
2024-09-30 18:31:19 +0530

    Create SolutionMoreQ28.c

---
+++ SolutionMoreQ28.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <time.h>
+
+int main()
+{
+    FILE *ptrlog = fopen("log.txt", "w");
+
+    time_t current_time = time(NULL);
+
+    struct tm *timeb = localtime(&current_time);
+
+    fprintf(ptrlog, "Current Date: %d-%d-%d\n", timeb->tm_year + 1900, timeb->tm_mon + 1, timeb->tm_mday);
+    fprintf(ptrlog, "Current Date: %s\n", asctime(timeb));
+
+    fclose(ptrlog);
+}
